4e4ec5cd97113805a62745c7269c5c05d9b76486
James-Alex Matthews <vamtiger@vamtiger.com>
2019-04-08 04:03:26 +0200

    vamtiger-node-typescript-commit

--- build/css/document-index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "vamtiger-responsive-svg [slot=vamtiger-responsive-svg]{width:100%;height:100%}\n/*# sourceMappingURL=document-index.ts.map */";
-export default _default;
--- build/css/document-index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = `vamtiger-responsive-svg [slot=vamtiger-responsive-svg]{width:100%;height:100%}
-/*# sourceMappingURL=document-index.ts.map */`;
-//# sourceMappingURL=document-index.js.map
